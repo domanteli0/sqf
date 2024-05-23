c3ad70186971db142e54f2ea363a1150a8dac33a
Domantelio✨ <domantelio@proton.me>
2024-05-23 03:23:50 +0300

    update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import Header from '../components/Header';
 import RecipeCard from '../components/RecipeCard';
-import '../styles/Home.css';
 
 const Home: React.FC = () => {
+    // Mock data
+    const recipes = [
+        { id: '1', title: 'Recipe 1', duration: '15 min' },
+        { id: '2', title: 'Recipe 2', duration: '10 min' },
+        { id: '3', title: 'Recipe 3', duration: '5 min' },
+    ];
+
     return (
         <div>
-            <Header />
+            <Header/>
             <main>
                 <h2>Best recipe results</h2>
-                <RecipeCard title="Recipe 1" duration="15 min" />
-                <RecipeCard title="Recipe 2" duration="10 min" />
-                <RecipeCard title="Recipe 3" duration="5 min" />
+                {recipes.map((recipe) => (
+                    <RecipeCard key={recipe.id} id={recipe.id} title={recipe.title} duration={recipe.duration}/>
+                ))}
             </main>
         </div>
     );
+
 };
+
 
 export default Home;
